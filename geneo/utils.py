bb3b469fcc465392d3d82ae2b0564dd894fa9145
DhruvGandhi31 <dhruvvgandhi3110@gmail.com>
2024-11-06 17:18:33 +0530

    changing matplotlib Agg

--- geneo/utils.py
+++ geneo/utils.py
@@ -1,17 +1,19 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import math
 import matplotlib
-matplotlib.use("TkAgg")
-import matplotlib.pyplot as plt
+matplotlib.use("Agg")
+
 
 def init_operators(op_type, number=8, size=5, centers=5, sigma=.1):
     """Returns a certain number of operators of type op_type. Now the kwargs are
     the one corresponding to the only kind of operator implemented (IENEO).
     """
-    return [op_type(size, sigma = sigma, centers = centers)
+    return [op_type(size, sigma=sigma, centers=centers)
             for i in range(number)]
 
-def bin_coeff(n,k):
+
+def bin_coeff(n, k):
     if k == n:
         cb = 1
     elif k == 1:
@@ -25,7 +27,8 @@ def bin_coeff(n,k):
         cb = a / (b*c)
     return int(cb)
 
-def plot_diagram(dgm, ax = None, show = False, labels = False):
+
+def plot_diagram(dgm, ax=None, show=False, labels=False):
     """Modified from dionysus to accept an axis instance as input.
     Plot the persistence diagram. The original code is stored at
     https://github.com/mrzv/dionysus/blob/master/bindings/python/dionysus/plot.py
@@ -49,8 +52,10 @@ def plot_diagram(dgm, ax = None, show = False, labels = False):
     if show:
         plt.show()
 
+
 def is_included(i1, i2):
     return i1[0] > i2[0] and i1[1] < i2[1]
+
 
 def get_order_of_magnitude(array):
     epsilon = 10e-6
